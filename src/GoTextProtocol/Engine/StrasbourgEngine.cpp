3272940a6303e641cb50b69bfdac96679d0faa72
Flavio Boss <flavio.boss@bluewin.ch>
2019-04-27 15:57:09 +0200

    strassbourg engine name command added

--- src/GoTextProtocol/Engine/StrasbourgEngine.cpp
+++ src/GoTextProtocol/Engine/StrasbourgEngine.cpp
@@ -1,21 +1,25 @@
 #include "StrasbourgEngine.h"
 #include "CommandExtractor.h"
+#include <NameCommand.h>
 #include <string>
 #include <string_view>
 
 using namespace Go::GoTextProtocol;
 
-auto unknown_command_response(const Command &command) noexcept {
-  if (command.id.has_value()) {
-    return std::string{"?" + std::to_string(command.id.value()) + " unknown command"};
-  } else {
-    return std::string{"? unknown command"};
-  }
-}
+auto handle_command(NameCommand &command) noexcept { return std::string{"HSLU Strasbourg Engine"}; }
 
 auto Go::GoTextProtocol::Engine::StrasbourgEngine::run(std::string_view view) noexcept -> std::string {
   view = view.substr(0, view.find_first_of("#\n"));
   auto command = Go::GoTextProtocol::Util::extractCommand(view);
 
-  return unknown_command_response(command);
+  std::string id{};
+  if (command.id.has_value()) {
+    id = std::to_string(command.id.value());
+  }
+
+  if ("name" == std::string{command.command.data()}) {
+    return std::string{"=" + id + " HSLU Strasbourg Engine"};
+  }
+
+  return std::string{"?" + id + " unknown command"};
 }
--- test/GoTextProtocol/Engine/StrasbourgEngineTest.cpp
+++ test/GoTextProtocol/Engine/StrasbourgEngineTest.cpp
@@ -4,6 +4,7 @@
 #include "StrasbourgEngine.h"
 
 namespace {
+
 TEST(StrasbourgEngine, empty) {
   Go::GoTextProtocol::Engine::StrasbourgEngine engine{};
   auto value = engine.run("");
@@ -20,6 +21,18 @@ TEST(StrasbourgEngine, bogusWhithId) {
   Go::GoTextProtocol::Engine::StrasbourgEngine engine{};
   auto value = engine.run("1234 asdgasd");
   EXPECT_EQ("?1234 unknown command", value);
+}
+
+TEST(StrasbourgEngine, NameCommandWithoutId) {
+  Go::GoTextProtocol::Engine::StrasbourgEngine engine{};
+  auto value = engine.run("name");
+  EXPECT_EQ("= HSLU Strasbourg Engine", value);
+}
+
+TEST(StrasbourgEngine, NameCommandWithId) {
+  Go::GoTextProtocol::Engine::StrasbourgEngine engine{};
+  auto value = engine.run("1234 name");
+  EXPECT_EQ("=1234 HSLU Strasbourg Engine", value);
 }
 
 } // namespace
