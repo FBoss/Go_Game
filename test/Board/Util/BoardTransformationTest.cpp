f041a060c63904b7ac27e70fba806fe1312d2398
Flavio Boss <flavio.boss@bluewin.ch>
2019-05-05 20:23:45 +0200

    run tests partial in constexpr context

--- test/Board/Util/BoardTransformationTest.cpp
+++ test/Board/Util/BoardTransformationTest.cpp
@@ -5,231 +5,234 @@
 
 namespace {
 
-    class BoardTransformationTest : public ::testing::Test {
+constexpr auto board_3x3() noexcept {
+  Go::TwoDimensionArrayBoard<3, 3> board{};
 
-    protected:
-        void SetUp() override {  }
-        void TearDown() override {}
+  board.set(0, 0, Go::Stone::empty);
+  board.set(0, 1, Go::Stone::white);
+  board.set(0, 2, Go::Stone::black);
+  board.set(1, 0, Go::Stone::white);
+  board.set(1, 1, Go::Stone::black);
+  board.set(1, 2, Go::Stone::empty);
+  board.set(2, 0, Go::Stone::white);
+  board.set(2, 1, Go::Stone::black);
+  board.set(2, 2, Go::Stone::empty);
 
-    public:
-        const Go::TwoDimensionArrayBoard<3,3> mBoard3x3 = [](){
-            Go::TwoDimensionArrayBoard<3,3> board{};
+  return board;
+}
 
-            board.set(0,0, Go::Stone::empty);
-            board.set(0,1, Go::Stone::white);
-            board.set(0,2, Go::Stone::black);
-            board.set(1,0, Go::Stone::white);
-            board.set(1,1, Go::Stone::black);
-            board.set(1,2, Go::Stone::empty);
-            board.set(2,0, Go::Stone::white);
-            board.set(2,1, Go::Stone::black);
-            board.set(2,2, Go::Stone::empty);
+constexpr auto board_3x3_90() noexcept {
+  Go::TwoDimensionArrayBoard<3, 3> board{};
 
-            return board;
-        }();
+  board.set(0, 0, Go::Stone::black);
+  board.set(0, 1, Go::Stone::empty);
+  board.set(0, 2, Go::Stone::empty);
+  board.set(1, 0, Go::Stone::white);
+  board.set(1, 1, Go::Stone::black);
+  board.set(1, 2, Go::Stone::black);
+  board.set(2, 0, Go::Stone::empty);
+  board.set(2, 1, Go::Stone::white);
+  board.set(2, 2, Go::Stone::white);
 
-        const Go::TwoDimensionArrayBoard<3,3> mBoard3x3_90Degree = [](){
-            Go::TwoDimensionArrayBoard<3,3> board{};
+  return board;
+}
 
-            board.set(0,0, Go::Stone::black);
-            board.set(0,1, Go::Stone::empty);
-            board.set(0,2, Go::Stone::empty);
-            board.set(1,0, Go::Stone::white);
-            board.set(1,1, Go::Stone::black);
-            board.set(1,2, Go::Stone::black);
-            board.set(2,0, Go::Stone::empty);
-            board.set(2,1, Go::Stone::white);
-            board.set(2,2, Go::Stone::white);
+constexpr auto board_3x3_180() noexcept {
+  Go::TwoDimensionArrayBoard<3, 3> board{};
 
-            return board;
-        }();
+  board.set(0, 0, Go::Stone::empty);
+  board.set(0, 1, Go::Stone::black);
+  board.set(0, 2, Go::Stone::white);
+  board.set(1, 0, Go::Stone::empty);
+  board.set(1, 1, Go::Stone::black);
+  board.set(1, 2, Go::Stone::white);
+  board.set(2, 0, Go::Stone::black);
+  board.set(2, 1, Go::Stone::white);
+  board.set(2, 2, Go::Stone::empty);
 
-        const Go::TwoDimensionArrayBoard<3,3> mBoard3x3_180Degree = [](){
-            Go::TwoDimensionArrayBoard<3,3> board{};
+  return board;
+}
 
-            board.set(0,0, Go::Stone::empty);
-            board.set(0,1, Go::Stone::black);
-            board.set(0,2, Go::Stone::white);
-            board.set(1,0, Go::Stone::empty);
-            board.set(1,1, Go::Stone::black);
-            board.set(1,2, Go::Stone::white);
-            board.set(2,0, Go::Stone::black);
-            board.set(2,1, Go::Stone::white);
-            board.set(2,2, Go::Stone::empty);
+constexpr auto board_3x3_270() noexcept {
+  Go::TwoDimensionArrayBoard<3, 3> board{};
 
-            return board;
-        }();
+  board.set(0, 0, Go::Stone::white);
+  board.set(0, 1, Go::Stone::white);
+  board.set(0, 2, Go::Stone::empty);
+  board.set(1, 0, Go::Stone::black);
+  board.set(1, 1, Go::Stone::black);
+  board.set(1, 2, Go::Stone::white);
+  board.set(2, 0, Go::Stone::empty);
+  board.set(2, 1, Go::Stone::empty);
+  board.set(2, 2, Go::Stone::black);
 
-        const Go::TwoDimensionArrayBoard<3,3> mBoard3x3_270Degree = [](){
-            Go::TwoDimensionArrayBoard<3,3> board{};
+  return board;
+}
 
-            board.set(0,0, Go::Stone::white);
-            board.set(0,1, Go::Stone::white);
-            board.set(0,2, Go::Stone::empty);
-            board.set(1,0, Go::Stone::black);
-            board.set(1,1, Go::Stone::black);
-            board.set(1,2, Go::Stone::white);
-            board.set(2,0, Go::Stone::empty);
-            board.set(2,1, Go::Stone::empty);
-            board.set(2,2, Go::Stone::black);
+constexpr auto board_4x4() noexcept {
+  Go::TwoDimensionArrayBoard<4, 4> board{};
 
-            return board;
-        }();
+  board.set(0, 0, Go::Stone::empty);
+  board.set(0, 1, Go::Stone::white);
+  board.set(0, 2, Go::Stone::empty);
+  board.set(0, 3, Go::Stone::white);
+  board.set(1, 0, Go::Stone::empty);
+  board.set(1, 1, Go::Stone::empty);
+  board.set(1, 2, Go::Stone::black);
+  board.set(1, 3, Go::Stone::empty);
+  board.set(2, 0, Go::Stone::empty);
+  board.set(2, 1, Go::Stone::white);
+  board.set(2, 2, Go::Stone::empty);
+  board.set(2, 3, Go::Stone::black);
+  board.set(3, 0, Go::Stone::black);
+  board.set(3, 1, Go::Stone::empty);
+  board.set(3, 2, Go::Stone::empty);
+  board.set(3, 3, Go::Stone::empty);
 
-        const Go::TwoDimensionArrayBoard<4,4> mBoard4x4 = [](){
-            Go::TwoDimensionArrayBoard<4,4> board{};
+  return board;
+}
 
-            board.set(0,0, Go::Stone::empty);
-            board.set(0,1, Go::Stone::white);
-            board.set(0,2, Go::Stone::empty);
-            board.set(0,3, Go::Stone::white);
-            board.set(1,0, Go::Stone::empty);
-            board.set(1,1, Go::Stone::empty);
-            board.set(1,2, Go::Stone::black);
-            board.set(1,3, Go::Stone::empty);
-            board.set(2,0, Go::Stone::empty);
-            board.set(2,1, Go::Stone::white);
-            board.set(2,2, Go::Stone::empty);
-            board.set(2,3, Go::Stone::black);
-            board.set(3,0, Go::Stone::black);
-            board.set(3,1, Go::Stone::empty);
-            board.set(3,2, Go::Stone::empty);
-            board.set(3,3, Go::Stone::empty);
+constexpr auto board_4x4_90() noexcept {
+  Go::TwoDimensionArrayBoard<4, 4> board{};
 
-            return board;
-        }();
+  board.set(0, 0, Go::Stone::white);
+  board.set(0, 1, Go::Stone::empty);
+  board.set(0, 2, Go::Stone::black);
+  board.set(0, 3, Go::Stone::empty);
+  board.set(1, 0, Go::Stone::empty);
+  board.set(1, 1, Go::Stone::black);
+  board.set(1, 2, Go::Stone::empty);
+  board.set(1, 3, Go::Stone::empty);
+  board.set(2, 0, Go::Stone::white);
+  board.set(2, 1, Go::Stone::empty);
+  board.set(2, 2, Go::Stone::white);
+  board.set(2, 3, Go::Stone::empty);
+  board.set(3, 0, Go::Stone::empty);
+  board.set(3, 1, Go::Stone::empty);
+  board.set(3, 2, Go::Stone::empty);
+  board.set(3, 3, Go::Stone::black);
 
-        const Go::TwoDimensionArrayBoard<4,4> mBoard4x4_90Degree = [](){
-            Go::TwoDimensionArrayBoard<4,4> board{};
+  return board;
+}
 
-            board.set(0,0, Go::Stone::white);
-            board.set(0,1, Go::Stone::empty);
-            board.set(0,2, Go::Stone::black);
-            board.set(0,3, Go::Stone::empty);
-            board.set(1,0, Go::Stone::empty);
-            board.set(1,1, Go::Stone::black);
-            board.set(1,2, Go::Stone::empty);
-            board.set(1,3, Go::Stone::empty);
-            board.set(2,0, Go::Stone::white);
-            board.set(2,1, Go::Stone::empty);
-            board.set(2,2, Go::Stone::white);
-            board.set(2,3, Go::Stone::empty);
-            board.set(3,0, Go::Stone::empty);
-            board.set(3,1, Go::Stone::empty);
-            board.set(3,2, Go::Stone::empty);
-            board.set(3,3, Go::Stone::black);
+constexpr auto board_4x4_180() noexcept {
+  Go::TwoDimensionArrayBoard<4, 4> board{};
 
-            return board;
-        }();
+  board.set(0, 0, Go::Stone::empty);
+  board.set(0, 1, Go::Stone::empty);
+  board.set(0, 2, Go::Stone::empty);
+  board.set(0, 3, Go::Stone::black);
+  board.set(1, 0, Go::Stone::black);
+  board.set(1, 1, Go::Stone::empty);
+  board.set(1, 2, Go::Stone::white);
+  board.set(1, 3, Go::Stone::empty);
+  board.set(2, 0, Go::Stone::empty);
+  board.set(2, 1, Go::Stone::black);
+  board.set(2, 2, Go::Stone::empty);
+  board.set(2, 3, Go::Stone::empty);
+  board.set(3, 0, Go::Stone::white);
+  board.set(3, 1, Go::Stone::empty);
+  board.set(3, 2, Go::Stone::white);
+  board.set(3, 3, Go::Stone::empty);
 
+  return board;
+}
 
-        const Go::TwoDimensionArrayBoard<4,4> mBoard4x4_180Degree = [](){
-            Go::TwoDimensionArrayBoard<4,4> board{};
+constexpr auto board_4x4_270() noexcept {
+  Go::TwoDimensionArrayBoard<4, 4> board{};
 
-            board.set(0,0, Go::Stone::empty);
-            board.set(0,1, Go::Stone::empty);
-            board.set(0,2, Go::Stone::empty);
-            board.set(0,3, Go::Stone::black);
-            board.set(1,0, Go::Stone::black);
-            board.set(1,1, Go::Stone::empty);
-            board.set(1,2, Go::Stone::white);
-            board.set(1,3, Go::Stone::empty);
-            board.set(2,0, Go::Stone::empty);
-            board.set(2,1, Go::Stone::black);
-            board.set(2,2, Go::Stone::empty);
-            board.set(2,3, Go::Stone::empty);
-            board.set(3,0, Go::Stone::white);
-            board.set(3,1, Go::Stone::empty);
-            board.set(3,2, Go::Stone::white);
-            board.set(3,3, Go::Stone::empty);
+  board.set(0, 0, Go::Stone::black);
+  board.set(0, 1, Go::Stone::empty);
+  board.set(0, 2, Go::Stone::empty);
+  board.set(0, 3, Go::Stone::empty);
+  board.set(1, 0, Go::Stone::empty);
+  board.set(1, 1, Go::Stone::white);
+  board.set(1, 2, Go::Stone::empty);
+  board.set(1, 3, Go::Stone::white);
+  board.set(2, 0, Go::Stone::empty);
+  board.set(2, 1, Go::Stone::empty);
+  board.set(2, 2, Go::Stone::black);
+  board.set(2, 3, Go::Stone::empty);
+  board.set(3, 0, Go::Stone::empty);
+  board.set(3, 1, Go::Stone::black);
+  board.set(3, 2, Go::Stone::empty);
+  board.set(3, 3, Go::Stone::white);
 
-            return board;
-        }();
+  return board;
+}
 
-        const Go::TwoDimensionArrayBoard<4,4> mBoard4x4_270Degree = [](){
-            Go::TwoDimensionArrayBoard<4,4> board{};
-
-            board.set(0,0, Go::Stone::black);
-            board.set(0,1, Go::Stone::empty);
-            board.set(0,2, Go::Stone::empty);
-            board.set(0,3, Go::Stone::empty);
-            board.set(1,0, Go::Stone::empty);
-            board.set(1,1, Go::Stone::white);
-            board.set(1,2, Go::Stone::empty);
-            board.set(1,3, Go::Stone::white);
-            board.set(2,0, Go::Stone::empty);
-            board.set(2,1, Go::Stone::empty);
-            board.set(2,2, Go::Stone::black);
-            board.set(2,3, Go::Stone::empty);
-            board.set(3,0, Go::Stone::empty);
-            board.set(3,1, Go::Stone::black);
-            board.set(3,2, Go::Stone::empty);
-            board.set(3,3, Go::Stone::white);
-
-            return board;
-        }();
-    };
-
-
-    TEST_F(BoardTransformationTest, emptyBoard) {
-        Go::TwoDimensionArrayBoard<3,3> begin{};
-        auto rot = Go::Util::rotate(begin);
-        EXPECT_EQ(begin, rot);
+TEST(BoardTransformationTest, emptyBoard) {
+  Go::TwoDimensionArrayBoard<3, 3> begin{};
+  auto rot = Go::Util::rotate(begin);
+  EXPECT_EQ(begin, rot);
     }
 
-
-
-    TEST_F(BoardTransformationTest, 3x3_90) {
-        auto rot = Go::Util::rotate(mBoard3x3);
-        EXPECT_EQ(mBoard3x3_90Degree, rot);
+    TEST(BoardTransformationTest, 3x3_90) {
+      constexpr auto rot = Go::Util::rotate(board_3x3());
+      EXPECT_EQ(board_3x3_90(), rot);
     }
 
-    TEST_F(BoardTransformationTest, 3x3_180) {
-        auto rot = Go::Util::rotate(mBoard3x3);
-        rot = Go::Util::rotate(rot);
-        EXPECT_EQ(mBoard3x3_180Degree, rot);
+    TEST(BoardTransformationTest, 3x3_180) {
+      constexpr auto rot = []() {
+        auto val = Go::Util::rotate(board_3x3());
+        return Go::Util::rotate(val);
+      }();
+      EXPECT_EQ(board_3x3_180(), rot);
     }
 
-    TEST_F(BoardTransformationTest, 3x3_270) {
-        auto rot = Go::Util::rotate(mBoard3x3);
-        rot = Go::Util::rotate(rot);
-        rot = Go::Util::rotate(rot);
-        EXPECT_EQ(mBoard3x3_270Degree, rot);
+    TEST(BoardTransformationTest, 3x3_270) {
+      constexpr auto rot = []() {
+        auto val = Go::Util::rotate(board_3x3());
+
+        val = Go::Util::rotate(val);
+        return Go::Util::rotate(val);
+      }();
+      EXPECT_EQ(board_3x3_270(), rot);
     }
 
-    TEST_F(BoardTransformationTest, 3x3_360) {
-        auto rot = Go::Util::rotate(mBoard3x3);
-        rot = Go::Util::rotate(rot);
-        rot = Go::Util::rotate(rot);
-        rot = Go::Util::rotate(rot);
-        EXPECT_EQ(mBoard3x3, rot);
+    TEST(BoardTransformationTest, 3x3_360) {
+      constexpr auto rot = []() {
+        auto val = Go::Util::rotate(board_3x3());
+
+        val = Go::Util::rotate(val);
+        val = Go::Util::rotate(val);
+        return Go::Util::rotate(val);
+      }();
+      EXPECT_EQ(board_3x3(), rot);
     }
 
-
-    TEST_F(BoardTransformationTest, 4x4_90) {
-        auto rot = Go::Util::rotate(mBoard4x4);
-        EXPECT_EQ(mBoard4x4_90Degree, rot);
+    TEST(BoardTransformationTest, 4x4_90) {
+      constexpr auto rot = Go::Util::rotate(board_4x4());
+      EXPECT_EQ(board_4x4_90(), rot);
     }
 
-    TEST_F(BoardTransformationTest, 4x4_180) {
-        auto rot = Go::Util::rotate(mBoard4x4);
-        rot = Go::Util::rotate(rot);
-        EXPECT_EQ(mBoard4x4_180Degree, rot);
+    TEST(BoardTransformationTest, 4x4_180) {
+      constexpr auto rot = []() {
+        auto val = Go::Util::rotate(board_4x4());
+        return Go::Util::rotate(val);
+      }();
+
+      EXPECT_EQ(board_4x4_180(), rot);
     }
 
-    TEST_F(BoardTransformationTest, 4x4_270) {
-        auto rot = Go::Util::rotate(mBoard4x4);
-        rot = Go::Util::rotate(rot);
-        rot = Go::Util::rotate(rot);
-        EXPECT_EQ(mBoard4x4_270Degree, rot);
+    TEST(BoardTransformationTest, 4x4_270) {
+      constexpr auto rot = []() {
+        auto val = Go::Util::rotate(board_4x4());
+        val = Go::Util::rotate(val);
+        return Go::Util::rotate(val);
+      }();
+      EXPECT_EQ(board_4x4_270(), rot);
     }
 
-    TEST_F(BoardTransformationTest, 4x4_360) {
-        auto rot = Go::Util::rotate(mBoard4x4);
-        rot = Go::Util::rotate(rot);
-        rot = Go::Util::rotate(rot);
-        rot = Go::Util::rotate(rot);
-        EXPECT_EQ(mBoard4x4, rot);
+    TEST(BoardTransformationTest, 4x4_360) {
+      constexpr auto rot = []() {
+        auto val = Go::Util::rotate(board_4x4());
+
+        val = Go::Util::rotate(val);
+        val = Go::Util::rotate(val);
+        return Go::Util::rotate(val);
+      }();
+      EXPECT_EQ(board_4x4(), rot);
     }
 
 } // namespace
