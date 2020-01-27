3ae032cccc56196036001df80790b1a5e8a1532c
Flavio Boss <flavio.boss@bluewin.ch>
2020-01-27 23:40:40 +0100

    Monte Carlo Tree Search added

--- src/Tournament/Tournament.cpp
+++ src/Tournament/Tournament.cpp
@@ -19,18 +19,14 @@ struct Move
 template <typename PLAYER, typename Engine>
 struct Player{
 
-    Move nextMove(GameEngine<Engine> & engine, Stone stone)
-    {
-        return static_cast<PLAYER *>(this)->nextMove(engine, stone);
-    }
-
+  Move nextMove(Engine &engine, Stone stone) { return static_cast<PLAYER *>(this)->nextMove(engine, stone); }
 };
 
 template <typename Engine>
 struct Contestant
 {
     Contestant(int rank, std::string contestant_name): ranking{rank}, name{contestant_name}{}
-    virtual Move nextMove(GameEngine<Engine> & engine, Stone stone) = 0;
+    virtual Move nextMove(Engine &engine, Stone stone) = 0;
 
     int ranking;
     std::string name;
@@ -40,45 +36,136 @@ template <typename Engine>
 struct ResignPlayer : public Player<ResignPlayer<Engine>, Engine>, Contestant<Engine>
 {
     ResignPlayer(): Contestant<Engine>{2000, "Resign"} {}
-    Move nextMove(GameEngine<Engine> &, Stone) override {return Move{MoveType::resign, {}};}
+    Move nextMove(Engine &, Stone) override { return Move{MoveType::resign, {}}; }
 };
 
 template <typename Engine>
 struct PassPlayer : public Player<ResignPlayer<Engine>, Engine>, Contestant<Engine>
 {
     PassPlayer(): Contestant<Engine>{2000, "Pass"} {}
-    Move nextMove(GameEngine<Engine> &, Stone) override {return Move{MoveType::pass, {}};}
+    Move nextMove(Engine &, Stone) override { return Move{MoveType::pass, {}}; }
 };
+
+static std::default_random_engine generator{};
+
+template <typename Engine> Move generateRandomMove(GameEngine<Engine> &engine, Stone stone) {
+  // std::random_device r;
+  //{r()};
+  std::uniform_int_distribution<int> rowDistribution(0, engine.board().getDimension().row - 1);
+  std::uniform_int_distribution<int> columnDistribution(0, engine.board().getDimension().column - 1);
+
+  int row = rowDistribution(generator);
+  int column = columnDistribution(generator);
+
+  for (int limit = 1000; not engine.is_play_valid(stone, row, column); --limit) {
+    row = rowDistribution(generator);
+    column = columnDistribution(generator);
+
+    if (limit <= 0) {
+      return Move{MoveType::pass, {}};
+    }
+  }
+  return Move{MoveType::move, {row, column}};
+}
 
 template <typename Engine>
 struct RandomPlayer : public Player<ResignPlayer<Engine>, Engine>, Contestant<Engine>
 {
     RandomPlayer(): Contestant<Engine>{2000, "Random"} {};
 
-    Move nextMove(GameEngine<Engine> & engine, Stone stone) override {
-        std::uniform_int_distribution<int> rowDistribution(0, engine.board().getDimension().row);
-        std::uniform_int_distribution<int> columnDistribution(0, engine.board().getDimension().row);
+    Move nextMove(Engine &engine, Stone stone) override { return generateRandomMove(engine, stone); }
 
+  private:
+    std::default_random_engine generator;
+};
 
-        int row = rowDistribution(generator);
-        int column = columnDistribution(generator);
+template <typename Engine>
+struct MonteCarloTreeSearchPlayer : public Player<MonteCarloTreeSearchPlayer<Engine>, Engine>, Contestant<Engine> {
+  MonteCarloTreeSearchPlayer() : Contestant<Engine>{2000, "MCTS"} {};
 
-        for(int limit = 1000; not engine.is_play_valid(stone, row, column); --limit)
-        {
-            row = rowDistribution(generator);
-            column = columnDistribution(generator);
+  Move nextMove(Engine &engine, Stone stone) override {
+    Node root{0, 0, engine, stone, {}, createNodes(engine, stone)};
 
-            if(limit <= 0)
-            {
-                return Move{MoveType::pass, {}};
+    for (auto &child : root.childeren) {
+      for (int i = 0; i < mRoundNumber; ++i) {
+        Engine playoutEngine = child.engine;
+        playout(playoutEngine, inverse(stone));
+        auto score = playoutEngine.score();
+        if (score.type == ScoreType::score) {
+          if (score.winner.value() == child.stone) {
+            ++child.win;
+          }
+        } else if (score.type == ScoreType::ongoing) {
+          if (child.stone == Stone::black) {
+            if (score.black > score.white) {
+              ++child.win;
             }
+          } else {
+            if (score.black < score.white) {
+              ++child.win;
+            }
+          }
         }
-
-        return Move{MoveType::move, {row, column}};
+        ++child.played;
+      }
     }
 
+    const auto best_move =
+        std::max_element(root.childeren.begin(), root.childeren.end(), [](const auto &lhs, const auto &rhs) { return lhs.win < rhs.win; });
+
+    return (*best_move).move;
+  }
+
 private:
-    std::default_random_engine generator;
+  struct Node {
+    int win;
+    int played;
+    Engine engine;
+    Stone stone;
+    Move move;
+    std::vector<Node> childeren;
+  };
+
+  std::vector<Node> createNodes(Engine engine, Stone stone) {
+    std::vector<Node> nodes{};
+
+    while (nodes.size() != mWidth) {
+      Node node{0, 0, engine, stone, generateRandomMove(engine, stone), {}};
+      applyMove(node.engine, node.move, stone);
+      nodes.emplace_back(node);
+    }
+
+    return nodes;
+  }
+
+  Stone inverse(Stone stone) { return (stone == Stone::black ? Stone::white : Stone::black); }
+
+  void playout(Engine &engine, Stone stone) {
+    while (engine.state() != GameState::game_over && engine.state() != GameState::end_of_game) {
+      auto move = generateRandomMove(engine, stone);
+      applyMove(engine, move, stone);
+      stone = inverse(stone);
+    }
+  }
+
+  void applyMove(Engine &engine, Move &move, Stone stone) {
+    if (move.type == MoveType::move) {
+      if (stone == Stone::black) {
+        engine.play_black(move.point.row, move.point.column);
+      } else {
+        engine.play_white(move.point.row, move.point.column);
+      }
+    } else {
+      if (stone == Stone::black) {
+        engine.play_black_pass();
+      } else {
+        engine.play_white_pass();
+      }
+    }
+  }
+
+  const unsigned int mWidth = 2;
+  const int mRoundNumber = 10;
 };
 
 template <typename Engine>
@@ -132,15 +219,16 @@ int main()
 {
     using namespace Go::GameEngine::Player;
 
-    constexpr int numberOfRounds = 100;
+    constexpr int numberOfRounds = 10;
 
     using UsedEngine = Go::GameEngine::StrassbourgEngine;
 
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts{};
     ResignPlayer<UsedEngine> resign{};
     PassPlayer<UsedEngine> pass{};
     RandomPlayer<UsedEngine> random{};
 
-    std::vector<Contestant<UsedEngine>*> contestants{&random, &random, &pass, &resign};
+    std::vector<Contestant<UsedEngine> *> contestants{&mcts, &random, &pass, &resign};
 
     if(contestants.size()% 2 != 0)
     {
