1f6b9c4263751275dd23e37507cc5b2ef8a9c1db
Flavio Boss <flavio.boss@bluewin.ch>
2020-01-30 00:42:21 +0100

    run competitions in parallel

--- src/Tournament/Tournament.cpp
+++ src/Tournament/Tournament.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <chrono>
 #include <cmath>
+#include <future>
 #include <iostream>
 #include <random>
 
@@ -89,13 +90,24 @@ struct MonteCarloTreeSearchPlayer : public Player<MonteCarloTreeSearchPlayer<Eng
 
   Move nextMove(Engine &engine, Stone stone) override {
     Node root = createTree(engine, stone);
+    mPlayer = stone;
 
     for (unsigned int i = 0; i < mPlayoutNumber; ++i) {
       playout(root);
     }
 
-    const auto best_move =
-        std::max_element(root.childeren.begin(), root.childeren.end(), [](const auto &lhs, const auto &rhs) { return lhs.win < rhs.win; });
+    const auto best_move = std::max_element(root.childeren.begin(), root.childeren.end(), [&](const auto &lhs, const auto &rhs) {
+      const auto branchScore = [&](const auto &branch) {
+        const double win = branch.win;
+        const double branchPlayed = branch.played;
+        const double totalPlayed = mPlayoutNumber;
+        const double exploration = 0.1; // std::log2(2);
+
+        return win / branchPlayed + exploration * std::sqrt(std::log(totalPlayed) / branchPlayed);
+      };
+
+      return branchScore(lhs) < branchScore(rhs);
+    });
 
     return (*best_move).move;
   }
@@ -161,12 +173,12 @@ private:
 
       auto score = playoutEngine.score();
       if (score.type == ScoreType::score) {
-        if (score.winner.value() == node.stone) {
+        if (score.winner.value() == mPlayer) {
           ++node.win;
           return 1;
         }
       } else if (score.type == ScoreType::ongoing) {
-        if (node.stone == Stone::black) {
+        if (mPlayer == Stone::black) {
           if (score.black > score.white) {
             ++node.win;
             return 1;
@@ -207,6 +219,7 @@ private:
   const unsigned int mNumberOfNodes;
   const unsigned int mDepth;
   const unsigned int mPlayoutNumber;
+  Stone mPlayer;
 };
 
 template <typename Engine>
@@ -260,21 +273,47 @@ int main()
 {
     using namespace Go::GameEngine::Player;
 
-    constexpr int numberOfRounds = 20;
+    constexpr int numberOfRounds = 750;
 
     using UsedEngine = Go::GameEngine::StrassbourgEngine;
 
-    MonteCarloTreeSearchPlayer<UsedEngine> mcts1{2, 1, 10};
-    MonteCarloTreeSearchPlayer<UsedEngine> mcts2{2, 1, 20};
-    MonteCarloTreeSearchPlayer<UsedEngine> mcts3{2, 1, 30};
-    MonteCarloTreeSearchPlayer<UsedEngine> mcts4{2, 1, 40};
-    MonteCarloTreeSearchPlayer<UsedEngine> mcts5{2, 1, 50};
-    MonteCarloTreeSearchPlayer<UsedEngine> mcts6{2, 1, 60};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts1{32, 4, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts2{32, 8, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts3{32, 16, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts4{32, 4, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts5{32, 8, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts6{32, 16, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts7{32, 4, 1000};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts8{32, 8, 1000};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts9{32, 16, 1000};
+
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts10{128, 4, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts11{128, 8, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts12{128, 16, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts13{128, 4, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts14{128, 8, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts15{128, 16, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts16{128, 4, 1000};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts17{128, 8, 1000};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts18{128, 16, 1000};
+
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts19{512, 4, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts20{512, 8, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts21{512, 16, 10};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts22{512, 4, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts23{512, 8, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts24{512, 16, 100};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts25{512, 4, 1000};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts26{512, 8, 1000};
+    MonteCarloTreeSearchPlayer<UsedEngine> mcts27{512, 16, 1000};
     ResignPlayer<UsedEngine> resign{};
     PassPlayer<UsedEngine> pass{};
     RandomPlayer<UsedEngine> random{};
 
-    std::vector<Contestant<UsedEngine> *> contestants{&mcts1, &mcts2, &mcts3, &mcts4, &mcts5, &mcts6, &random};
+    std::vector<Contestant<UsedEngine> *> contestants{
+        &mcts1,  &mcts2,  &mcts3,  &mcts4,  &mcts5,  &mcts6,  &mcts7,  &mcts8,  &mcts9,  &mcts10, &mcts11, &mcts12, &mcts13, &mcts14,
+        &mcts15, &mcts16, &mcts17, &mcts18, &mcts19, &mcts20, &mcts21, &mcts22, &mcts23, &mcts24, &mcts25, &mcts26, &mcts27, &random};
+    // std::vector<Contestant<UsedEngine> *> contestants{&mcts1, &mcts2, &mcts3, &mcts4};
 
     if(contestants.size()% 2 != 0)
     {
@@ -289,50 +328,49 @@ int main()
     {
         std::shuffle(contestants.begin(), contestants.end(), rng);
 
-    for(auto iterator = contestants.begin(); iterator != contestants.end(); iterator=iterator+2)
-    {
-        const auto player_black = *iterator;
-        const auto player_white = *(iterator+1);
-        const auto result = compete(player_black, player_white);
+        const auto gamePlay = [](auto player_black, auto player_white) {
+          const auto result = compete(player_black, player_white);
 
-        float estimate_black = 1/(1+std::pow(10, (player_white->ranking - player_black->ranking)/400));
-        float estimate_white = 1-estimate_black;
+          float estimate_black = 1 / (1 + std::pow(10, (player_white->ranking - player_black->ranking) / 400));
+          float estimate_white = 1 - estimate_black;
 
-        float result_value_black = [&](){
-            if(result.type == Go::GameEngine::ScoreType::jigo)
-            {
-                return 0.5;
+          float result_value_black = [&]() {
+            if (result.type == Go::GameEngine::ScoreType::jigo) {
+              return 0.5;
             }
-            if(result.type == Go::GameEngine::ScoreType::win_by_resign || result.type == Go::GameEngine::ScoreType::score)
-            {
-            if(result.winner == Go::Stone::black)
-            {
+            if (result.type == Go::GameEngine::ScoreType::win_by_resign || result.type == Go::GameEngine::ScoreType::score) {
+              if (result.winner == Go::Stone::black) {
                 return 1.0;
-            }else
-            {
+              } else {
                 return 0.0;
+              }
             }
-            }
-            if(result.type == Go::GameEngine::ScoreType::ongoing)
-            {
-                if (result.black == result.white)
-                {
-                    return 0.5;
-                }else if( result.black>result.white)
-                {
-                    return 1.0;
-                }else
-                {
-                    return 0.0;
-                }
+            if (result.type == Go::GameEngine::ScoreType::ongoing) {
+              if (result.black == result.white) {
+                return 0.5;
+              } else if (result.black > result.white) {
+                return 1.0;
+              } else {
+                return 0.0;
+              }
             }
             return 0.0;
-        }();
+          }();
 
-        float result_value_white = 1-result_value_black;
+          float result_value_white = 1 - result_value_black;
 
-        player_black->ranking = player_black->ranking + 50*(result_value_black-estimate_black);
-        player_white->ranking = player_white->ranking + 50*(result_value_white-estimate_white);
+          player_black->ranking = player_black->ranking + 50 * (result_value_black - estimate_black);
+          player_white->ranking = player_white->ranking + 50 * (result_value_white - estimate_white);
+        };
+
+        auto a2 = std::async(gamePlay, &pass, &resign);
+        std::vector<decltype(a2)> gamesInThisRound{};
+
+        for (auto iterator = contestants.begin(); iterator != contestants.end(); iterator = iterator + 2) {
+          const auto player_black = *iterator;
+          const auto player_white = *(iterator + 1);
+          gamesInThisRound.emplace_back(std::async(gamePlay, player_black, player_white));
+          // gamePlay(player_black, player_white);
     }
     }
 
